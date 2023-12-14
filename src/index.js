e1394367f98012fa8eebbd558331d79b5782fbfd
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-14 13:34:38 -0600

    render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const express from 'express'
-const app = express()
-const port = 3000
+import express from 'express'
 
-app.get('/', (req, res) => {
-  res.send({ msg: 'Hello World!' })
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req,res) => {
+    res.send({msg: "Hello There"})
 })
+
 
 app.listen(port)
